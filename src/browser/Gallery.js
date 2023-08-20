99f331a188e35016e41ed205b5d8a2d30580c482
Uniweb CMS <uniwebcms@proximify.ca>
2023-08-20 14:50:31 +0200

    Clean up

--- src/browser/Gallery.js
+++ src/browser/Gallery.js
@@ -1,38 +1,9 @@
-// import React from 'react';
-// import { Gallery, Item } from 'react-photoswipe-gallery';
-// import 'photoswipe/dist/photoswipe.css';
-
-// /**
-//  * Gallery of component preview images.
-//  * @param {Array} images - Array of image objects. Each object should have the following properties: [src, width, height]
-//  * @returns
-//  */
-// export default function ({ images }) {
-//     return (
-//         <Gallery id='image-gallery'>
-//             <div className='imageGallery'>
-//                 {images.map((image, index) => {
-//                     const [src, width, height] = image;
-
-//                     return (
-//                         <Item key={index} original={src} thumbnail={src} width={width} height={height}>
-//                             {({ ref, open }) => <img ref={ref} onClick={open} src={src} />}
-//                         </Item>
-//                     );
-//                 })}
-//             </div>
-//         </Gallery>
-//     );
-// }
-
-// Below is an attempt to eliminate the need for the <BrowserOnly/> wrapper within the user's tutorial project.
-
 import React from 'react';
-// import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/dist/photoswipe.css';
 
 /**
- * Logic to avoid React hydration problems.
+ * Logic to avoid React hydration problems. It eliminate the need for the 
+ * <BrowserOnly/> wrapper within the user's tutorial project.
  *
  * The Gallery component can't run on server-side rendering (SSR) mode
  * used by Docusaurus to convert a React website to a static version.
@@ -44,6 +15,9 @@ import 'photoswipe/dist/photoswipe.css';
  *
  * On first client-side render, we need to render exactly as the server rendered
  * isBrowser is set to true only after a successful hydration.
+ * 
+ * Note that even importing the library is not SSR compatible, so the library
+ * is added dynamically on mount, which doesn't happen on SSR mode.
  *
  * @see https://github.com/facebook/docusaurus/blob/main/packages/docusaurus/src/client/browserContext.tsx
  *
@@ -62,8 +36,9 @@ function useGetComponents() {
 
 /**
  * Gallery of component preview images.
- * @param {Array} images - Array of image objects. Each object should have the following properties: [src, width, height]
- * @returns
+ * @param {Array} images - Array of image objects. Each object should 
+ * have the following properties: [src, width, height]
+ * @returns {function} A React Gallery component.
  */
 export default function ({ images }) {
     const components = useGetComponents();
